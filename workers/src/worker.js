930088d22df9ae12ed3b4413797422c44d1b954a
tomo-x7 <158121497+tomo-x7@users.noreply.github.com>
2024-07-28 18:04:21 +0900

    workers

---
+++ workers/src/worker.js
@@ -0,0 +1,20 @@
+/**
+ * Welcome to Cloudflare Workers! This is your first worker.
+ *
+ * - Run "npm run dev" in your terminal to start a development server
+ * - Open a browser tab at http://localhost:8787/ to see your worker in action
+ * - Run "npm run deploy" to publish your worker
+ *
+ * Learn more at https://developers.cloudflare.com/workers/
+ */
+
+
+export default {
+  async fetch(request, env, ctx) {
+    if(request.method==="GET"){
+      return new Response(JSON.stringify({hoge:"huga"}))
+    }
+    const data = await request.json(); 
+    return new Response(JSON.stringify(data));
+  },
+};
