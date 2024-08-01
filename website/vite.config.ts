4f424acc5617f67ec13add1f3e0d522155529c04
tomo-x7 <158121497+tomo-x7@users.noreply.github.com>
2024-08-02 05:52:07 +0900

    ログインページ追加

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -1,14 +1,25 @@
 /** @type {import('vite').UserConfig} */
-export default {
-    server:{
-        cors: false,
-        proxy:{
-            "/api": {
-                target: "https://schedulesky.tomo-x.workers.dev/",
-                changeOrigin: true,
-                secure: true,
-                rewrite: (path) => path.replace(/^\/api/, '')
-            }
-        }
-    }
-}
+import { resolve } from "node:path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+	server: {
+		cors: false,
+		proxy: {
+			"/api": {
+				target: "https://schedulesky.tomo-x.workers.dev/",
+				changeOrigin: true,
+				secure: true,
+				rewrite: (path) => path.replace(/^\/api/, ""),
+			},
+		},
+	},
+	build: {
+		rollupOptions: {
+			input: {
+				main: resolve(__dirname, "index.html"),
+				login: resolve(__dirname, "login/index.html"),
+			},
+		},
+	},
+});
