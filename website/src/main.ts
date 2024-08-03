ef7657f87acc5be91ce83cb09c0da66a007db9bd
tomo-x7 <158121497+tomo-x7@users.noreply.github.com>
2024-08-04 04:03:58 +0900

    TLA

--- website/src/main.ts
+++ website/src/main.ts
@@ -1,6 +1,8 @@
 import "./main.css";
-const profile = await fetch("/api/getprofile");
-if (profile.status === 401) {
-	location.href = "/login/";
-}
-(<HTMLImageElement>document.getElementById("avatar")).src=(await profile.json()).avatar
+(async () => {
+	const profile = await fetch("/api/getprofile");
+	if (profile.status === 401) {
+		location.href = "/login/";
+	}
+	(<HTMLImageElement>document.getElementById("avatar")).src = (await profile.json()).avatar;
+})();
