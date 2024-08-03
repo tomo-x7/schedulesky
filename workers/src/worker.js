227dfeeaf112fa9ea198a397e5fcb5caf00e5bec
tomo-x7 <158121497+tomo-x7@users.noreply.github.com>
2024-08-04 02:28:27 +0900

    レスポンスのbodyの修正

--- workers/src/worker.js
+++ workers/src/worker.js
@@ -11,11 +11,11 @@ export default {
 		if (method === "POST" && path === "/login") {
 			//バリデーション
 			if (type !== "application/json") {
-				return new Response({ error: "Content-Type must be 'application/json'" }, { status: 400 });
+				return createresponse({ error: "Content-Type must be 'application/json'" }, 400);
 			}
 			const { endpoint, identifier, password } = await request.json();
 			if (!URL.canParse(`https://${endpoint}/`)) {
-				return new Response({ error: "bad endpoint" }, { status: 400 });
+				return createresponse({ error: "bad endpoint" }, 400);
 			}
 			const res = await fetch(`https://${endpoint}/xrpc/com.atproto.server.createSession`, {
 				body: JSON.stringify({ identifier, password }),
@@ -30,14 +30,14 @@ export default {
 			const { accessJwt, refreshJwt } = await res.json();
 			const header = new Headers();
 			setcookie(header, refreshJwt, accessJwt);
-			return new Response({ message: "success" }, { status: 200, headers: header });
+			return createresponse({ message: "success" }, 200, header);
 		}
 		//プロフィールの取得
 		if (method === "GET" && path === "/getprofile") {
 			//セッションを取得してtokenがなければ認証を要求
 			const session = getsession(cookie);
 			if (!session) {
-				return new Response({ error: "require auth" }, { status: 401 });
+				return createresponse({ error: "require auth" }, 401);
 			}
 			const data = await fetch(`${session.endpoint}/xrpc/app.bsky.actor.getProfile?actor=${encodeURIComponent(session.DID)}`, {
 				headers: { Authorization: `Bearer ${session.accesstoken}` },
@@ -45,7 +45,7 @@ export default {
 			data.headers.append("Set-Cookie");
 		}
 		//どれにも該当しなければ404
-		return new Response({ error: "not found" }, { status: 404 });
+		return createresponse({ error: "not found" }, 404);
 	},
 };
 /**
@@ -137,4 +137,14 @@ function setcookie(header, refreshJwt, accessJwt) {
 	const refreshJwtdata = tokenParser(refreshJwt);
 	header.append("Set-Cookie", serialize("refreshtoken", refreshJwt, { httpOnly: true, secure: true, expires: refreshJwtdata.exp }));
 	header.append("Set-Cookie", serialize("accesstoken", accessJwt, { httpOnly: true, secure: true, expires: accessJwtdata.exp }));
+}
+/**
+ * @param {Object} body
+ * @param {number?} status
+ * @param {Headers?} header
+ */
+function createresponse(body, status = 200, header = undefined) {
+	const newheader = header ?? new Headers();
+	newheader.append("Content-Type", "application/json");
+	return new Response(JSON.stringify(body), { status: status ?? 200, headers: newheader });
 }
