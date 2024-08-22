30ebbfe0357fc09d64fc109d784f55d674ff904d
tomo-x7 <158121497+tomo-x7@users.noreply.github.com>
2024-08-22 23:19:45 +0900

    test

--- website/_api/callback.ts
+++ website/_api/callback.ts
@@ -12,12 +12,18 @@ export default async function GET(req: VercelRequest, res: VercelResponse) {
 		const { agent, state } = await client.callback(params);
 		const sessionID = Buffer.from(crypto.getRandomValues(new Uint32Array(10)).buffer).toString("base64url");
 		redis.setredis(`mysession_${sessionID}`, agent.did, 7200);
+		const profile = (await agent.getProfile({ actor: agent.did })).data;
+
 		return res
 			.appendHeader(
 				"Set-Cookie",
 				serialize("session", sessionID, { httpOnly: true, secure: true, sameSite: "lax", maxAge: 7200, path: "/" }),
 			)
-			.redirect(302, "/");
+			.send(`<script>
+				localStorage.setItem("handle",${profile.handle});
+				localStorage.setItem("icon",${profile.avatar});
+				window.location="/";
+				</script>`);
 	} catch {
 		return res.status(400).send("failed auth");
 	}
