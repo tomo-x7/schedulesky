6758adb0d3cd8a3f88a787d72e129cc793655f4d
tomo-x7 <158121497+tomo-x7@users.noreply.github.com>
2024-08-25 18:48:17 +0900

    delete old

--- website-old/src/early.ts
+++
@@ -1,8 +0,0 @@
-import "./main.css";
-document.addEventListener("DOMContentLoaded", async () => {
-	const profile = await fetch("/api/getprofile");
-	if (profile.status === 401) {
-		location.href = "/login/";
-	}
-	(<HTMLImageElement>document.getElementById("avatar")).src = (await profile.json()).avatar;
-});
